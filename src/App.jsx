76f0e382c723c0d67a29116c1e1f0d84daa0fe9e
Pratyush Shrestha <121381876+PratyushShrestha-web@users.noreply.github.com>
2025-09-16 16:51:31 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
-import Body from './Components/Body/body'
+import Body from './Components/Body/Body'
 import Features from './Components/Contentss/Features'
 import TestimonialSection from './Components/Comments/TestimonialSection'
 import Footer from './Components/Last/Footer'
