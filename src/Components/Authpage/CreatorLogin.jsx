e6ed42e927f035c645893586ce0485b8fca66cad
Avinash Pokhrel <106798140+xetri@users.noreply.github.com>
2025-09-17 08:22:12 +0000

    Add Sign In with google

--- src/Components/Authpage/CreatorLogin.jsx
+++ src/Components/Authpage/CreatorLogin.jsx
@@ -1,41 +1,33 @@
 import React from 'react';
 import CenteredContainer from './Centercontainer'; // adjust path as needed
 import logo from '../../assets/googleicon.png';
+import "./CreatorLogin.css"
+
+import { auth } from "../../firebase";
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
 const CreatorLogin = () => {
+  
+  async function SignInWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    const res = await signInWithPopup(auth, provider);
+  }
+
   return (
     <CenteredContainer
       title="Creator Login"
       subtitle="Sign in to manage your creator profile and content"
-      footer="For supporters: No account needed! Just connect your wallet to tip creators."
+      footer="For supporters: Just connect your wallet to tip creators"
     >
-      <form>
-        <label className="input-label" htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          placeholder="creator@example.com"
-          className="input-field"
-        />
-
-        <label className="input-label" htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          placeholder="••••••••"
-          className="input-field"
-        />
-
-        <button type="submit" className="signin-button">Sign In</button>
-        
-        <button type="submit" className="signin-button2">
-            <img src={logo} alt="logo" className='icon'/> Sign In with Google</button>
-         
-      </form>
-
-      <div className="login-links">
-        <p><a href="/forgot-password">Forgot your password?</a></p>
-        <p className='Ac'>Don't have an account? <a href="/signup">Sign up</a></p>
+      <div>
+        <div className="login-buttons">
+          <button type="button" className="signin-button2" onClick={SignInWithGoogle}>
+            <img src={logo} alt="logo" className='icon'/>
+            <p>
+              Sign In with Google
+            </p>
+          </button>
+        </div>
       </div>
     </CenteredContainer>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -8,5 +9,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 
 export default app;
