f2c76fa0a63bdbec7e1226d095474dc5b3a446e0
Avinash Pokhrel <106798140+xetri@users.noreply.github.com>
2025-09-19 19:41:58 +0000

    Clean 404 page

--- src/pages/Auth/CreatorLogin.jsx
+++ src/pages/Auth/CreatorLogin.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import CenteredContainer from './Centercontainer'; // adjust path as needed
+import { useNavigate } from 'react-router';
+import { useWallet } from "@solana/wallet-adapter-react";
+
+import { ServerURL} from "../../config";
+import CenteredContainer from './Centercontainer';
 import logo from '../../assets/googleicon.png';
 import "./CreatorLogin.css"
 
-import { ServerURL } from "../../config";
-
 import { auth } from "../../firebase";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { useAuth } from '../../context/AuthContext';
 
 const CreatorLogin = () => {
+  const { user, logout } = useAuth();
+  const goto = useNavigate();
+
   async function SignInWithGoogle() {
     const provider = new GoogleAuthProvider();
-
-    console.log(auth.currentUser)
 
     const res = await signInWithPopup(auth, provider);
     const token = await res.user.getIdToken();
@@ -23,30 +26,53 @@ const CreatorLogin = () => {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ token })
-    })
-      .then(res => {
-        console.log(auth.currentUser)
-      })
-      .catch(e => {
-        console.log(e.message)
-    })
+    }).then(_ => {
+      goto("/dashboard");
+    }).catch(e => {
+      console.log(e.message)
+    });
+  }
 
+  const { connected, disconnect } = useWallet();
+
+  async function SignOut() {
+    try {
+      await logout();
+      if (connected) {
+        await disconnect();
+      };
+    } catch (err) {
+      console.error("Failed to logout:", err);
+    }
   }
 
   return (
     <CenteredContainer
       title="Creator Login"
-      subtitle="Sign in to manage your creator profile and content"
       footer="For supporters: Just connect your wallet to tip creators"
     >
       <div>
         <div className="login-buttons">
-          <button type="button" className="signin-button2" onClick={SignInWithGoogle}>
-            <img src={logo} alt="logo" className='icon'/>
-            <p>
-              Sign In with Google
-            </p>
-          </button>
+            {
+              user ? 
+                <div>
+                  <p>
+                    Already Logged In as {user.displayName}
+                  </p>
+                  <button type="button" className="signin-button2" onClick={SignOut}>
+                    <p>
+                      Logout
+                    </p>
+                  </button>
+                </div>
+              : 
+              <button type="button" className="signin-button2" onClick={SignInWithGoogle}>
+                  <img src={logo} alt="logo" className='icon'/>
+                  <p>
+                    Login with Google
+                  </p>
+              </button>
+            }
         </div>
       </div>
     </CenteredContainer>
