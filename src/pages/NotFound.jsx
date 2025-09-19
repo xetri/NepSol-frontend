114db22df310600213160b25ad5f238161e65616
Avinash Pokhrel <106798140+xetri@users.noreply.github.com>
2025-09-19 19:44:07 +0000

    Done

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,21 @@
-import React from "react"
+import React from "react";
+import { useNavigate } from "react-router";
+import "./404.css";
 
-export default function NotFound() {
-    return (<>
-        <h1>404 - Not Found</h1>
-    </>)
+function NotFound() {
+  const goto = useNavigate();
+
+  return (
+    <div className="notfound-container">
+      <h1 className="notfound-title">404</h1>
+      <p className="notfound-text">
+        Oops! The page you’re looking for doesn’t exist.
+      </p>
+      <button className="primary-btn" onClick={() => goto("/")}>
+        Go Home
+      </button>
+    </div>
+  );
 }
+
+export default NotFound;
